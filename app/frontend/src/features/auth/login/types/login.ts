215ed5d00d2827b8c1a661d058227d88fde69ecd
MailsonSousa88 <legionslmax86@gmail.com>
2026-07-09 08:19:13 -0300

    feat: adiciona conexão com rota de login

--- app/frontend/src/features/auth/login/types/login.ts
+++ app/frontend/src/features/auth/login/types/login.ts
@@ -2,3 +2,19 @@ export type LoginFormValues = {
   email: string
   password: string
 }
+
+export type LoginPayload = {
+  email: string
+  password: string
+}
+
+export type LoginResponse = {
+  accessToken: string
+  refreshToken: string
+  profile: {
+    id: string
+    name: string
+    phone: string
+    document?: string
+  }
+}
